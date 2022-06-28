ece3767d2f3903d8d054b111ad87fd9f9710bb48
Tejasri554 <97598370+Tejasri554@users.noreply.github.com>
2022-06-28 16:41:06 +0530

    n=int(input()) t=n p=n*n s=0 l=len(str(n)) for i in range(1,l+1):     r=n%10     s=s*10+r     n=n//10 p1=s*s t1=p1 l1=len(str(p1)) s1=0 for j in range(1,l1+1):     r1=p1%10     s1=s1*10+r1     p1=p1//10 if p==s1:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,21 @@
 n=int(input())
-p1=n*n
-l=len(str(n))
+t=n
+p=n*n
 s=0
+l=len(str(n))
 for i in range(1,l+1):
-    s=s*10+n%10
+    r=n%10
+    s=s*10+r
     n=n//10
-r=s*s
-l1=len(str(r))
-m=0
+p1=s*s
+t1=p1
+l1=len(str(p1))
+s1=0
 for j in range(1,l1+1):
-    m=m*10+r%10
-    r=r//10
-if m==p1:
+    r1=p1%10
+    s1=s1*10+r1
+    p1=p1//10
+if p==s1:
     print(True)
 else:
     print(False)
