e2aa1e98b04170f2ce6a6fa8e9c03dcbcdf11a04
Tejasri554 <97598370+Tejasri554@users.noreply.github.com>
2022-07-01 22:30:45 +0530

    n=int(input()) s=n*n sum=0 for i in range(len(str(s))):     r=s%10     sum=sum+r     s=s//10 if sum==n:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,12 +1,11 @@
 n=int(input())
-p=n*n
-l=len(str(p))
-s=0
-for i in range(1,l+1):
-    r=p%10
-    s=s+r
-    p=p//10
-if n==s:
+s=n*n
+sum=0
+for i in range(len(str(s))):
+    r=s%10
+    sum=sum+r
+    s=s//10
+if sum==n:
     print("Neon Number")
 else:
     print("Not Neon Number")
